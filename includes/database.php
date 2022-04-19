1788655399882808f666925c5b2ce5effa6799fc
joanis96 <jeanjoanis96@gmail.com>
2022-04-19 13:30:10 -0400

    db modif

--- includes/database.php
+++ includes/database.php
@@ -1,3 +1,7 @@
 <?php
 $con = mysqli_connect('localhost','root','','jaifaimv3');
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+  }
 ?>
